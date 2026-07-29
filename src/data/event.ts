e33447dd4eff7125c47e46a47ab319c9a716209a
Rafael Teixeira <rafael.teixeira@libertyti.com.br>
2026-07-29 13:38:11 -0300

    chore: update event address

--- src/data/event.ts
+++ src/data/event.ts
@@ -4,7 +4,7 @@ export const eventInfo = {
   date: "Inserir data",
   time: "Inserir horário",
   venue: "Local da comemoração",
-  address: "Coordenadas: -15.842728, -48.027527",
+  address: "Av. Pau Brasil, 18 - Águas Claras",
   mapsUrl: "https://maps.app.goo.gl/zqHGezPNspHpHWjNA",
   mapEmbedUrl: "https://www.google.com/maps?q=-15.842728,-48.027527&output=embed",
   reference: "",
