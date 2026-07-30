5c07a0d764b7c8b76779bcd00e08dfd00f036ab0
Rafael Teixeira <rafael.teixeira@libertyti.com.br>
2026-07-29 21:27:34 -0300

    content: add remaining song lyrics

--- src/data/program.ts
+++ src/data/program.ts
@@ -51,7 +51,35 @@ Faz-me mais grato a Ti, mais grato a Ti!`,
     type: "song",
     title: "Hino 597 — Graças Dou",
     artist: "Harpa Cristã",
-    content: "",
+    content: `Graças dou por esta vida
+Pelo bem que revelou
+Graças dou pelo futuro
+E por tudo que passou
+
+Pelas bênçãos derramadas
+Pela dor, pela aflição
+Pela graça revelada
+Graças dou pelo perdão
+
+Graças pelo azul-celeste
+E por nuvens que há também
+Pelas rosas do caminho
+Por espinhos que elas têm
+
+Pela escuridão da noite
+Pela estrela que brilhou
+Pela prece respondida
+Pelo sonho que falhou
+
+Pela cruz e o sofrimento
+E por toda provação
+Pelo amor que é sem medida
+Pela paz no coração
+
+Pela lágrima vertida
+Pelo alívio que é sem par
+Pelo dom da eterna vida
+Sempre graças hei de dar`,
   },
   {
     id: "meu-tributo",
@@ -59,7 +87,57 @@ Faz-me mais grato a Ti, mais grato a Ti!`,
     type: "song",
     title: "Meu Tributo",
     artist: "Vitorino Silva",
-    content: "",
+    content: `Como agradecer
+Por tudo que fizeste a mim
+Não merecedor
+Mas provaste o seu amor sem fim
+
+As vozes de um milhão de anjos
+Não expressam a minha gratidão
+Tudo o que sou e o que almejo ser
+Eu devo tudo a ti
+
+A Deus seja a glória
+A Deus seja a glória
+A Deus seja a glória
+Por tudo o que fez por mim
+
+Com seu sangue lavou-me
+Seu poder levantou-me
+A Deus seja a glória
+Para sempre, amém
+
+Quero viver para ti
+Tua vontade obedecer
+E se o aplauso eu receber
+No calvário irei me gloriar
+
+Com seu sangue lavou-me
+Seu poder levantou-me
+A Deus seja a glória
+Para sempre, amém
+
+A Deus seja a glória
+A Deus seja a glória
+A Deus seja a glória
+Por tudo o que fez por mim
+
+Com seu sangue lavou-me
+Seu poder levantou-me
+A Deus seja a glória
+Para sempre, amém
+
+Quero viver para Ti
+Tua vontade obedecer
+E se o aplauso eu receber
+No calvário irei me gloriar
+
+Com Seu sangue lavou-me
+Seu poder levantou-me
+A Deus seja a glória
+Para sempre, amém
+
+Para sempre, amém`,
   },
   {
     id: "rosto-de-cristo",
@@ -67,21 +145,112 @@ Faz-me mais grato a Ti, mais grato a Ti!`,
     type: "song",
     title: "Rosto de Cristo",
     artist: "Sarah Farias",
-    content: "",
+    content: `Sempre que eu leio a história de Cristo
+Eu fico a pensar com grande emoção
+No privilégio que muitos tiveram de ver
+O Seu rosto, sentir Suas mãos
+
+Eu também teria a mesma alegria
+De vê-lo bem perto, bem junto a mim
+Olhar em seus olhos, serenos e meigos
+Oh, como eu seria, tão feliz assim
+
+Queria saber como era o Seu rosto
+Embora eu sinta que era mui lindo
+Inspirava fé e também confiança
+E dava a todos um gozo infindo
+
+Ao ver as gravuras dos quadros pintados
+Daquilo que dizem ser o meu Senhor
+Meu ser não aceita o que está na tela
+É falsa a inspiração do pintor
+
+Não creio, não creio num Cristo vencido
+Cheio de amargura, semblante de dor
+Eu creio num Cristo de rosto alegre
+Eu creio num Cristo que é vencedor
+
+Um dia também, eu o verei face a face
+E assim eu creio, pela minha fé
+
+Oh, aleluia!
+Verei o Seu rosto, verei a Jesus, como Ele é
+
+Eu queria saber como era o Seu rosto
+Embora eu sinta que era mui lindo
+Inspirava fé e também confiança
+E dava a todos um gozo infindo
+
+Ah, um dia também eu o verei
+Eu o verei face a face
+Sim, eu o verei face a face
+E assim eu creio pela minha fé
+
+Oh, aleluia!
+Verei o Seu rosto, verei a Jesus, como Ele é
+Oh, aleluia!
+Verei o Seu rosto, verei a Jesus, como Ele é
+Ele é`,
   },
   {
     id: "divino-companheiro",
     order: 6,
     type: "song",
     title: "Divino Companheiro",
-    content: "",
+    content: `Divino companheiro no caminho
+Sua presença sinto logo ao transitar
+Já dissipaste toda sombra
+Já tenho luz, a luz bendita do amor
+
+Fica, Senhor, já se faz tarde
+Tens meu coração para pousar
+Faz em mim morada permanente
+Fica, Senhor, fica, Senhor, meu Salvador
+
+A sombra da noite se aproxima
+E nela o tentador vai chegar
+Não, não me deixe só no caminho
+Ajuda-me, ajuda-me até chegar
+
+Fica, Senhor, já se faz tarde
+Tens meu coração para pousar
+Faz em mim morada permanente
+Fica, Senhor, fica, Senhor, meu Salvador
+
+Fica, Senhor, já se faz tarde
+Tens meu coração para pousar
+Faz em mim morada permanente
+Fica, Senhor, fica, Senhor, meu Salvador
+
+Faz em mim morada permanente
+Fica, Senhor, fica, Senhor, meu Salvador`,
   },
   {
     id: "cem-ovelhas",
     order: 7,
     type: "song",
     title: "Cem Ovelhas",
-    content: "",
+    content: `Eram cem ovelhas juntas no aprisco
+Eram cem ovelhas que, amante, cuidou
+Porém, numa tarde, ao contá-las todas
+Lhe faltava uma, lhe faltava uma e triste chorou
+
+As noventa e nove deixou no aprisco
+E pelas montanhas a buscá-la foi
+A encontrou gemendo, tremendo de frio
+Curou suas feridas, pôs logo em seus ombros e ao redil voltou
+
+Esta mesma história volta a repetir-se
+Pois muitas ovelhas perdidas estão
+Mas ainda hoje, o Pastor amado
+Chora tuas feridas, chora tuas feridas e quer te salvar
+
+As noventa e nove deixou no aprisco
+E pelas montanhas a buscá-la foi
+A encontrou gemendo, tremendo de frio
+Curou suas feridas, pôs logo em seus ombros e ao redil voltou
+
+Curou suas feridas, pôs logo em seus ombros e ao redil voltou`,
   },
   {
     id: "deus-e-fiel",
@@ -89,6 +258,74 @@ Faz-me mais grato a Ti, mais grato a Ti!`,
     type: "song",
     title: "Deus é Fiel",
     artist: "Nani Azevedo",
-    content: "",
+    content: `Sim, Deus é fiel
+Para cumprir toda palavra dita a mim
+Deus é fiel
+Deus é fiel
+
+Sim, Deus é fiel
+Para cumprir toda promessa feita a mim
+Deus é fiel
+Deus é fiel
+
+Eu não morrerei
+Enquanto o Senhor não cumprir em mim
+Todos os sonhos
+Que Ele mesmo sonhou pra mim
+
+Eu quero viver
+Em santidade e adoração
+Pois é só Dele
+Somente Dele o meu coração
+
+Eu não morrerei
+Enquanto o Senhor não cumprir em mim
+Todos os sonhos
+Que Ele mesmo sonhou pra mim
+
+Eu quero viver
+Em santidade e adoração
+Pois é só Dele
+Somente Dele o meu coração
+
+Sim, Deus é fiel
+Para cumprir toda palavra dita a mim
+Deus é fiel
+Deus é fiel
+
+Sim, Deus é fiel
+Para cumprir toda promessa feita a mim
+Deus é fiel
+Deus é fiel
+
+Eu não morrerei
+Enquanto o Senhor não cumprir em mim
+Todos os sonhos
+Que Ele mesmo sonhou pra mim
+
+Eu quero viver
+Em santidade e adoração
+Pois é só Dele
+Somente Dele o meu coração
+
+Eu não morrerei
+Enquanto o Senhor não cumprir em mim
+Todos os sonhos
+Que Ele mesmo sonhou pra mim
+
+Eu quero viver
+Em santidade e adoração
+Pois é só Dele
+Somente Dele o meu coração
+
+Tu não morrerás
+Enquanto o Senhor não cumprir em ti
+Todos os sonhos
+Que Ele mesmo sonhou pra ti
+
+Então viverás
+Em santidade e adoração
+Pois é só Dele
+Somente Dele o teu coração`,
   },
 ];
